0900c8cc67f4463d747485e38824697bbbc29039
canguleiro <camilotorquato@hotmail.com>
2026-09-16 10:43:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_VERSION = "em-pauta-v7";
+const CACHE_VERSION = "criart-v8";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
 const STATIC_ASSETS = [
   "./manifest.json",
   "./icon.svg",
+  "./icon-criart-192.png",
+  "./icon-criart-512.png",
   "./css/app.css"
 ];
 
@@ -35,13 +37,10 @@ self.addEventListener("fetch", event => {
   const request = event.request;
   const url = new URL(request.url);
 
-  // Só tratamos requisições do próprio Em Pauta.
   if (url.origin !== location.origin) {
     return;
   }
 
-  // Nunca cachear HTML nem JavaScript do aplicativo.
-  // Isso garante que Ctrl+R receba sempre a versão atual.
   if (
     request.method !== "GET" ||
     request.destination === "script" ||
